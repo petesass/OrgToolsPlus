5e41f9c2e922df83f08d8cfad1eba7f9048edae4
Dr. Peter Sass <124244105+petesass@users.noreply.github.com>
2023-02-21 03:17:08 -0700

    Update OTPMerge.py

--- OTPMerge.py
+++ OTPMerge.py
@@ -4,36 +4,36 @@ import shutil
 # prompt user for directory path
 dir_path = input("Enter the path of the directory to search: ")
 
-# create the Merger folder if it does not exist
-merger_path = os.path.join(dir_path, 'Merger')
-if not os.path.exists(merger_path):
-    os.makedirs(merger_path)
+# create the OTPMerge folder if it does not exist
+OTPMerge_path = os.path.join(dir_path, 'OTPMerge')
+if not os.path.exists(OTPMerge_path):
+    os.makedirs(OTPMerge_path)
 
 # loop through all subfolders in the directory
 for root, dirs, files in os.walk(dir_path):
-    # ignore the Merger folder
-    if root == merger_path:
+    # ignore the OTPMerge folder
+    if root == OTPMerge_path:
         continue
 
-    # move all files in the current subfolder to the Merger folder
+    # move all files in the current subfolder to the OTPMerge folder
     for file in files:
         file_path = os.path.join(root, file)
-        shutil.move(file_path, os.path.join(merger_path, file))
+        shutil.move(file_path, os.path.join(OTPMerge_path, file))
 
-print("Files merged into the Merger folder.")
+print("Files merged into the OTPMerge folder.")
 
-# check if the user wants to delete empty Scrubber folders
-delete_scrubber = input("Do you want to delete the empty Scrubber folders? (y/n) ")
-if delete_scrubber == 'y':
+# check if the user wants to delete empty OTPScrub folders
+delete_OTPScrub = input("Do you want to delete the empty OTPScrub folders? (y/n) ")
+if delete_OTPScrub == 'y':
     # loop through all subfolders in the directory
     for root, dirs, files in os.walk(dir_path):
-        # ignore the Merger folder and any subfolders inside it
-        if root == merger_path or root.startswith(merger_path):
+        # ignore the OTPMerge folder and any subfolders inside it
+        if root == OTPMerge_path or root.startswith(OTPMerge_path):
             continue
         
         for d in dirs:
-            # delete the folder if it has "Scrubber" in its name and is empty
-            if "Scrubber" in d and not os.listdir(os.path.join(root, d)):
+            # delete the folder if it has "OTPScrub" in its name and is empty
+            if "OTPScrub" in d and not os.listdir(os.path.join(root, d)):
                 shutil.rmtree(os.path.join(root, d))
 
-print("Scrubber folders deleted (if specified).")
+print("OTPScrub folders deleted (if specified).")
