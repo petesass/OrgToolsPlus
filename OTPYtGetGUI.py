c312ed2bdf3354d9a49cc731779e83304e969bea
Dr. Peter Sass <124244105+petesass@users.noreply.github.com>
2023-03-10 15:37:56 -0700

    Update OTPYtGetGUI.py

--- OTPYtGetGUI.py
+++ OTPYtGetGUI.py
@@ -5,6 +5,7 @@ import os
 from tkinter import messagebox
 from tkinter import filedialog
 
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -19,6 +20,10 @@ class Application(tk.Frame):
         self.youtube_link_entry = tk.Entry(self)
         self.youtube_link_entry.pack()
 
+        self.keep_video_var = tk.BooleanVar()
+        self.keep_video_checkbox = tk.Checkbutton(self, text="Keep video file", variable=self.keep_video_var)
+        self.keep_video_checkbox.pack()
+
         self.download_button = tk.Button(self, text="Download", command=self.download_audio)
         self.download_button.pack()
 
@@ -26,47 +31,48 @@ class Application(tk.Frame):
         self.open_folder_button.pack()
 
     def download_audio(self):
-        # Step 1: Get the YouTube video URL from the user
         youtube_link = self.youtube_link_entry.get()
 
-        # Step 2: Create a Pytube object for the YouTube video
         youtube_object = pytube.YouTube(youtube_link)
 
-        # Step 3: Get the highest quality audio stream from the YouTube video
         audio_stream = youtube_object.streams.filter(only_audio=True).order_by('abr').desc().first()
 
-        # Step 4: Download the audio stream to a local file
         audio_file = audio_stream.download()
 
-        # Step 5: Get the title of the YouTube video
         video_title = youtube_object.title
-
-        # Step 6: Replace special characters in the title and create a file name
         filename = video_title.replace('|', '').replace('"', '').replace('/', '').replace('\\', '').replace('*', '').replace('?', '').replace('<', '').replace('>', '').replace(':', '').replace('.', '').strip() + '.mp3'
 
-        # Step 7: Create a folder called "youtubeGet" in the "documents" directory if it does not already exist
         folder_path = os.path.join(os.path.expanduser('~'), 'documents', 'youtubeGet')
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        # Step 8: Create the full path for the output MP3 file
         output_file_path = os.path.join(folder_path, filename)
 
-        # Step 9: Convert the downloaded audio file to MP3 format using MoviePy library and save to the output file path
         mp_audio = mp.AudioFileClip(audio_file)
         mp_audio.write_audiofile(output_file_path, verbose=False)
 
-        # Step 10: Delete the downloaded audio and video files
+        if self.keep_video_var.get():
+            video_stream = youtube_object.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            video_file = video_stream.download()
+
+            video_filename = video_title.replace('|', '').replace('"', '').replace('/', '').replace('\\', '').replace('*', '').replace('?', '').replace('<', '').replace('>', '').replace(':', '').replace('.', '').strip() + '.mp4'
+            video_output_path = os.path.join(folder_path, video_filename)
+
+            mp_video = mp.VideoFileClip(video_file)
+            mp_video.audio.write_audiofile(output_file_path, verbose=False)
+            final_clip = mp_video.set_audio(mp_audio)
+            final_clip.write_videofile(video_output_path, audio_codec='aac', verbose=False)
+
+            os.remove(video_file)
+
         os.remove(audio_file)
-        for file in os.listdir(os.getcwd()):
-            if file.endswith('.webm'):
-                os.remove(file)
 
         messagebox.showinfo("Success", "Download successful!")
 
     def open_folder(self):
         folder_path = os.path.join(os.path.expanduser('~'), 'documents', 'youtubeGet')
         os.system(f'start "" "{folder_path}"')
+
 
 root = tk.Tk()
 app = Application(master=root)
