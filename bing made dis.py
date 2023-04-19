66fec994fe0a588309ba21b45a362f8a8fdaa9e4
Dr. Peter Sass <124244105+petesass@users.noreply.github.com>
2023-04-18 21:03:09 -0600

    Update bing made dis.py

--- bing made dis.py
+++ bing made dis.py
@@ -1,39 +1,29 @@
-#That sounds like a very interesting and challenging project. I can try to generate some code for you, but I cannot guarantee that it will work or sound good. Here is what I came up with:
-#
-#```python
-# Importing modules
 import tkinter as tk
 import random
 import mido
 
-# Creating a GUI window
 window = tk.Tk()
 window.title("MIDI Melody Generator")
 window.geometry("600x400")
 
-# Creating a label to display the title
 title_label = tk.Label(window, text="MIDI Melody Generator", font=("Arial", 24))
 title_label.pack()
 
-# Creating a frame to hold the options
 options_frame = tk.Frame(window)
 options_frame.pack()
 
-# Creating a label and a scale for the tempo
 tempo_label = tk.Label(options_frame, text="Tempo (BPM):")
 tempo_label.grid(row=0, column=0)
 tempo_scale = tk.Scale(options_frame, from_=40, to=200, orient=tk.HORIZONTAL)
 tempo_scale.set(120)
 tempo_scale.grid(row=0, column=1)
 
-# Creating a label and a scale for the length
 length_label = tk.Label(options_frame, text="Length (bars):")
 length_label.grid(row=1, column=0)
 length_scale = tk.Scale(options_frame, from_=4, to=32, orient=tk.HORIZONTAL)
 length_scale.set(8)
 length_scale.grid(row=1, column=1)
 
-# Creating a label and a listbox for the key
 key_label = tk.Label(options_frame, text="Key:")
 key_label.grid(row=2, column=0)
 key_listbox = tk.Listbox(options_frame, height=12, selectmode=tk.MULTIPLE)
@@ -43,7 +33,6 @@ for key in keys:
 key_listbox.selection_set(0)
 key_listbox.grid(row=2, column=1)
 
-# Creating a label and a listbox for the mode
 mode_label = tk.Label(options_frame, text="Mode:")
 mode_label.grid(row=3, column=0)
 mode_listbox = tk.Listbox(options_frame, height=2)
@@ -53,7 +42,6 @@ for mode in modes:
 mode_listbox.selection_set(0)
 mode_listbox.grid(row=3, column=1)
 
-# Creating a label and a listbox for the composer
 composer_label = tk.Label(options_frame, text="Composer:")
 composer_label.grid(row=4, column=0)
 composer_listbox = tk.Listbox(options_frame, height=4)
@@ -63,38 +51,30 @@ for composer in composers:
 composer_listbox.selection_set(3)
 composer_listbox.grid(row=4, column=1)
 
-# Creating a button to generate the melody
 generate_button = tk.Button(window, text="Generate", font=("Arial", 16))
 generate_button.pack()
 
-# Defining some constants and variables
 NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 NOTE_NUMBERS = {name: i for i, name in enumerate(NOTE_NAMES)}
 MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
 MINOR_SCALE = [0, 2, 3, 5, 7, 8, 10]
 CHORDS = {
-    # Major triads
     (0, 4): [0], # I
     (5, 9): [5], # V
     (7, 11): [7], # vii°
-    # Minor triads
     (2, 5): [2], # ii
     (3 ,7): [3], # iii
     (9 ,12): [9], # vi
-    # Diminished triads
     (11 ,14): [11], # vii°
-    # Augmented triads
     (4 ,8): [4], # IV+
 }
 CHORD_PROGRESSIONS = {
-    # Major key progressions
     "Major": [
         [(0,), (5,), (9,), (5,), (0,), (5,), (9,), (5,),], # I-V-vi-V-I-V-vi-V
         [(0,), (5,), (9,), (4,), (0,), (5,), (9,), (4,),], # I-V-vi-IV-I-V-vi-IV
         [(0,), (4,), (5,), (0,), (9,), (4,), (5,), (9,),], # I-IV-V-I-vi-IV-V-vi
         [(0,), (4,), (2,), (5,), (9,), (2,), (5,), (0,),], # I-IV-ii-V-vi-ii-V-I
     ],
-    # Minor key progressions
     "Minor": [
         [(9,), (2,), (5,), (9,), (3,), (7,), (11,), (3,)], # vi-ii-V-vi-iii-vii°-I-iii        
         [(9,), (2,), (5,), (9,), (3,), (7,), (0, 4), (3,)], # vi-ii-V-vi-iii-vii°-I+-iii
@@ -102,25 +82,20 @@ CHORD_PROGRESSIONS = {
         [(9,), (7,), (11,), (9,), (2,), (7,), (11,), (2,)], # vi-vii°-I-vi-ii-vii°-I-ii
     ],
 }
-# Defining a function to generate a random melody
 def generate_melody():
-    # Getting the options from the GUI
     tempo = tempo_scale.get()
     length = length_scale.get()
     key = key_listbox.get(key_listbox.curselection()[0])
     mode = mode_listbox.get(mode_listbox.curselection()[0])
     composer = composer_listbox.get(composer_listbox.curselection()[0])
 
-    # Creating a MIDI file and a track
     midi_file = mido.MidiFile()
     midi_file.ticks_per_beat = 480
     track = mido.MidiTrack()
     midi_file.tracks.append(track)
 
-    # Adding a tempo message
     track.append(mido.MetaMessage("set_tempo", tempo=mido.bpm2tempo(tempo)))
 
-    # Defining some variables for the melody generation
     scale = MAJOR_SCALE if mode == "Major" else MINOR_SCALE
     tonic = NOTE_NUMBERS[key]
     notes = [tonic + note for note in scale] + [tonic + 12]
@@ -128,28 +103,23 @@ def generate_melody():
     velocity = 64
     time = 0
 
-    # Choosing a chord progression based on the composer or randomly
     if composer == "Random":
         composer = random.choice(list(CHORD_PROGRESSIONS.keys()))
     progression = random.choice(CHORD_PROGRESSIONS[composer])
 
-    # Looping through the bars and chords
     for bar in range(length):
         chord = progression[bar % len(progression)]
         chord_notes = [notes[note] + octave * 12 for note in chord]
 
-        # Looping through the beats and subdivisions
         for beat in range(4):
             subdivision = random.randint(1, 4)
             duration = midi_file.ticks_per_beat // subdivision
 
-            # Choosing a note from the chord or the scale randomly
             if random.random() < 0.8:
                 note = random.choice(chord_notes)
             else:
                 note = random.choice(notes) + octave * 12
 
-            # Adjusting the octave if the note is too high or low
             if note > 84:
                 octave -= 1
                 note -= 12
@@ -157,38 +127,28 @@ def generate_melody():
                 octave += 1
                 note += 12
 
-            # Adding a note on and off message to the track
             track.append(mido.Message("note_on", note=note, velocity=velocity, time=time))
             track.append(mido.Message("note_off", note=note, velocity=velocity, time=duration))
             time = 0
 
-    # Adding an end of track message to the track
     track.append(mido.MetaMessage("end_of_track"))
 
-    # Saving the MIDI file to a temporary file
     midi_file.save("temp.mid")
 
-# Defining a function to play the MIDI file using pygame
 def play_melody():
     import pygame
 
-    # Initializing pygame and its mixer
     pygame.init()
     pygame.mixer.init()
 
-    # Loading and playing the MIDI file
     pygame.mixer.music.load("temp.mid")
     pygame.mixer.music.play()
 
-# Binding the generate button to the generate melody function
 generate_button.config(command=generate_melody)
 
-# Creating another button to play the melody
 play_button = tk.Button(window, text="Play", font=("Arial", 16))
 play_button.pack()
 
-# Binding the play button to the play melody function
 play_button.config(command=play_melody)
 
-# Starting the main loop of the GUI window
 window.mainloop()
